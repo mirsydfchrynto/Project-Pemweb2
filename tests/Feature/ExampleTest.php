02d804d93eb43cdd177c30863dc3fad418a4f628
mirsydfchrynto <mirsyadf1805@gmail.com>
2025-05-12 16:04:11 +0700

    UTS Pemweb 2 (23090111)

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,4 @@
-<?php
-
-it('returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
-});
+// it('returns a successful response', function () {
+//     $response = $this->get('/');
+//     $response->assertStatus(200);
+// });
